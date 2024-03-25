7fe6f74678e2f9cf67dc27a19b42c09cb2d055b7
Kushim-Jiang <894857367@qq.com>
2024-03-25 14:32:57 +0800

    Routine update

---
+++ src/build_yaml.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from pandas import DataFrame, read_excel
+
+
+input_dir = Path(__file__).parent.parent / "input"
+xlsx_dir = input_dir / "abstract_shape.xlsx"
+xlsx_file = read_excel(io=xlsx_dir, sheet_name=None)
+
+names_dict = {"main": "main", "ExtA": "a", "ExtB": "b"}
+
+
+def build_historical():
+    for sheet_name, file_name in names_dict.items():
+        result = ""
+        sheet: DataFrame = xlsx_file.get(sheet_name)
+        data: DataFrame = sheet.loc[:, ["char", "his", "his.1"]]
+        lines = data.to_csv(sep="\t", na_rep="").split("\n")
+
+        store_char = ""
+        for line in lines[1:]:
+            try:
+                num, char, abst, comm = line.split("\t")
+
+                if (abst + comm).strip() != "":
+                    store_char = char if char.strip() != "" else store_char
+                    result += f"{store_char}\t{abst}\t{comm}".rstrip() + "\n"
+            except ValueError:
+                pass
+
+        with open(input_dir / f"history_{file_name}.txt", "w", encoding="utf-8") as f:
+            f.write(result)
+
+
+def build_now():
+    for sheet_name, file_name in names_dict.items():
+        result = ""
+        sheet: DataFrame = xlsx_file.get(sheet_name)
+        data: DataFrame = sheet.loc[:, ["char.1", "con", "recon", "comm"]]
+        lines = data.to_csv(sep="\t", na_rep="").split("\n")
+
+        store_char = ""
+        for line in lines[1:]:
+            try:
+                num, char, abst, reco, comm = line.split("\t")
+                if (abst + reco + comm).strip() != "":
+                    store_char = char if char.strip() != "" else store_char
+                    result += f"{store_char}\t{abst}\t{reco}\t{comm}".rstrip() + "\n"
+            except ValueError:
+                pass
+
+        with open(input_dir / f"abstract_{file_name}.txt", "w", encoding="utf-8") as f:
+            f.write(result)
+
+
+def main():
+    build_historical()
+    build_now()
+
+
+if __name__ == "__main__":
+    main()
